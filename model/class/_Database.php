dbb0f10be90d1b941323743984f57740422a1413
T.Leclere <thibaud.leclere@gmail.com>
2021-12-16 19:59:05 +0100

    init db class

--- model/class/_Database.php
+++ model/class/_Database.php
@@ -4,11 +4,11 @@
 class Database
 {
     private const HOST = '127.0.0.1';
-    private const PORT = '3306';
+    private const PORT = '3307';
     private const CHARSET = 'utf8';
-    private const LOGIN = 'u934752231_AppCasseTete';
+    private const LOGIN = 'AppJCT';
     private const PASS = 'sk4#Srvmpcrci';
-    private const DBNAME = 'u934752231_jeancassetete';
+    private const DBNAME = 'jeancassetete';
     private static ?PDO $connection = null;
 
     public static function GetConnection():PDO
